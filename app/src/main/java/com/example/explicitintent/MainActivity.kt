8aef26e6ec1d1ab8177333226088dc22e80fecb1
Pradeep Singh <88311782+gprs1022@users.noreply.github.com>
2023-02-05 19:45:38 +0530

    Explicit Intents

---
+++ app/src/main/java/com/example/explicitintent/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.explicitintent
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val btnIntent = findViewById<Button>(R.id.button)
+
+        btnIntent.setOnClickListener {
+
+            intent = Intent(applicationContext,Secondactivity::class.java)
+
+            startActivity(intent)
+        }
+    }
+}
